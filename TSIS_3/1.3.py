ebcbc1089e53215e8459dd455a3a7322ef55d790
Sayakhatova <s.naz03@mail.ru>
2021-02-15 02:16:00 +0600

    first commit

--- TSIS_3/1.3.py
+++ TSIS_3/1.3.py
@@ -1,1 +1,4 @@
-#https://informatics.mccme.ru/mod/statements/view.php?id=4535&chapterid=3751#1
+#https://informatics.mccme.ru/mod/statements/view.php?id=4535&chapterid=3751#
+a=input().split()
+b=input().split()
+print(set(a).intersection(b))
